148d9fab86bfc1c0381708310bb667d2f29e8abc
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-09-05 22:45:36 +0200

    Multithreaded

--- src/colatz.rs
+++ src/colatz.rs
@@ -1,10 +1,12 @@
 use std::cmp::max;
 
+use rayon::prelude::*;
+
 pub type T = i64;
 
 fn do_colatz(is: &mut [T]) {
     for i in is {
-        *i = if *i % 2 == 0 { *i / 2 } else { 3 * *i + 1 };
+        *i = if *i % 2 == 0 { *i } else { 3 * *i + 1 } / 2;
     }
 }
 
@@ -23,6 +25,28 @@ pub fn do_n_colatz(is: &mut [T], maxs: &mut [T], n: usize) -> usize {
         do_colatz(is);
         update_max(is, maxs);
     }
+
+    amt_done(is)
+}
+
+pub fn do_n_colatz_threaded(
+    is: &mut [T],
+    maxs: &mut [T],
+    n: usize,
+) -> usize {
+    // let chunk_size = is.len() / nth;
+    const CHUNK_SIZE: usize = 1024;
+
+    let (is_c, is_r) = is.as_chunks_mut::<CHUNK_SIZE>();
+    let (maxs_c, maxs_r) = maxs.as_chunks_mut::<CHUNK_SIZE>();
+
+    is_c.par_iter_mut().zip_eq(maxs_c.par_iter_mut()).for_each(
+        |(is_c, maxs_c)| {
+            do_n_colatz(is_c, maxs_c, n);
+        },
+    );
+
+    do_n_colatz(is_r, maxs_r, n);
 
     amt_done(is)
 }
--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,29 @@ fn main() {
     let n_nums = args.next().unwrap().parse().unwrap();
     let n_iter = args.next().unwrap().parse().unwrap();
 
+    let threads = if let Some(t) = args.next().and_then(|x| x.parse().ok()) {
+        t
+    } else {
+        // num_cpus::get();
+        1
+    };
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(threads)
+        .build_global()
+        .unwrap_or(());
+
     let mut nums: Vec<_> = (1..=n_nums as T).collect();
     let mut maxs = vec![0; n_nums];
 
     let t = Instant::now();
 
-    let amt_done = do_n_colatz(&mut nums, &mut maxs, n_iter);
+    let amt_done = do_n_colatz_threaded(&mut nums, &mut maxs, n_iter);
 
     let t = t.elapsed();
 
     println!("Amt done: {amt_done}");
+    let max_reached = maxs.iter().max().unwrap();
+    println!("Max reached: {max_reached}");
     println!("Took {t:?}");
 }
